6ffa2bea5acbf3ca40c9c2c1585e8edbaab21a56
freakcoder1 <35484642+freakcoder1@users.noreply.github.com>
2018-01-16 15:07:07 +0530

    Add files via upload

---
+++ 3rdprogramsubstration.py
@@ -0,0 +1,5 @@
+#subtraction between 2 number
+a=200
+b=100
+c=a-b
+print("subtraction of",a,"-",b,"=",c)
---
+++ my2ndprogram.py
@@ -0,0 +1,6 @@
+#adding 2 numbers
+a=10
+b=20
+c=a+b
+print("addition of",a,"and",b,"is",c)
+      
