cbd43fe56b2cae105413102dd68e0f7ea3f2e843
freakcoder1 <35484642+freakcoder1@users.noreply.github.com>
2018-01-30 15:20:28 +0530

    Add files via upload

---
+++ booleanoperater.py
@@ -0,0 +1,15 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> two
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    two
+NameError: name 'two' is not defined
+>>> 1==1 and 2==2
+True
+>>> 1==1 and 2==4
+False
+>>> 1==10 or 2==2
+True
+>>> 
---
+++ random.py
@@ -0,0 +1,8 @@
+import random
+print(random.randint(1,100))
+import random
+print(random.randint(1,100))
+import random
+print(random.randint(1,100))
+import random
+print(random.randint(1,100))
